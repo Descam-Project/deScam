c93c0660d6a3b9d5cb8eb6da73d9924ab336e351
captvelsky <lesmananabil@gmail.com>
2022-05-18 19:15:51 +0700

    add splash & auth layout

---
+++ app/src/main/java/com/captvelsky/descam/AuthActivity.kt
@@ -0,0 +1,20 @@
+package com.captvelsky.descam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.captvelsky.descam.databinding.ActivityAuthBinding
+
+class AuthActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAuthBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAuth.setOnClickListener {
+
+        }
+    }
+}
