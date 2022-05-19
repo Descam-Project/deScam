47003ee9dc3cc557a2eeb545d68ebad353efd35c
captvelsky <lesmananabil@gmail.com>
2022-05-20 06:10:38 +0700

    prepare dagger hilt

--- app/src/main/java/com/captvelsky/descam/ResultFragment.kt
+++ app/src/main/java/com/captvelsky/descam/ResultFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.captvelsky.descam.databinding.FragmentResultBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ResultFragment : Fragment() {
 
     private var _binding: FragmentResultBinding? = null
