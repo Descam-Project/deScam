cc168e591585d8935b2bcc6d82f389668b3e1e1c
buginugroho <buginugroho17@gmail.com>
2022-06-01 03:04:16 +0700

    Add OCR function to extract text from image

--- app/src/main/java/com/captvelsky/descam/ui/fragment/ScanFragment.kt
+++ app/src/main/java/com/captvelsky/descam/ui/fragment/ScanFragment.kt
@@ -2,18 +2,25 @@ package com.captvelsky.descam.ui.fragment
 
 import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.util.forEach
 import com.captvelsky.descam.R
 import com.captvelsky.descam.databinding.FragmentScanBinding
 import com.captvelsky.descam.helper.uriToFile
+import com.google.android.gms.vision.Frame
+import com.google.android.gms.vision.text.TextRecognizer
 import dagger.hilt.android.AndroidEntryPoint
 import java.io.File
+import java.lang.StringBuilder
 import java.util.*
 
 @AndroidEntryPoint
@@ -22,6 +29,9 @@ class ScanFragment : Fragment() {
     private var _binding: FragmentScanBinding? = null
     private val binding get() = _binding
     private var getFile: File? = null
+
+    private lateinit var bitmap: Bitmap
+    private lateinit var text: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,6 +56,7 @@ class ScanFragment : Fragment() {
     private fun setupButon() {
         binding?.openGalleryButton?.setOnClickListener { openGallery() }
         binding?.uploadButtonImageView?.setOnClickListener { uploadPicture() }
+        binding?.extractTextButton?.setOnClickListener { extractText(bitmap) }
     }
 
     private fun openGallery() {
@@ -63,6 +74,7 @@ class ScanFragment : Fragment() {
             val selectedImg : Uri = result.data?.data as Uri
             val myImgFile = uriToFile(selectedImg, requireActivity())
             getFile = myImgFile
+            bitmap = BitmapFactory.decodeFile(myImgFile.path)
 
             binding?.previewImageView?.setImageURI(selectedImg)
             adjustButton()
@@ -71,14 +83,36 @@ class ScanFragment : Fragment() {
 
     private fun uploadPicture() {
         // Fungsi upload diletakkan di sini
+        Toast.makeText(requireActivity(), text, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun extractText(bitmap: Bitmap) {
+        val textRecognizer = TextRecognizer.Builder(requireActivity()).build()
+        if (!textRecognizer.isOperational){
+            Toast.makeText(requireActivity(), "Error", Toast.LENGTH_SHORT).show()
+        } else {
+            val frame = Frame.Builder().setBitmap(bitmap).build()
+            val textBlockSparseArray = textRecognizer.detect(frame)
+            val stringBuilder = StringBuilder()
+            textBlockSparseArray.forEach { _, value ->
+                val textBlock = value
+                stringBuilder.append(textBlock.value)
+                stringBuilder.append("\n")
+            }
+            text = stringBuilder.toString()
+            binding?.extractedTextTextView?.text = stringBuilder.toString()
+            binding?.uploadButtonImageView?.visibility = View.VISIBLE
+        }
     }
 
     private fun adjustButton() {
         if (getFile == null) {
-            binding?.uploadButtonImageView?.visibility = View.GONE
+            binding?.extractTextButton?.visibility = View.GONE
+            binding?.extractedTextCardView?.visibility = View.GONE
         } else {
             binding?.openGalleryButton?.text = resources.getString(R.string.choose_another_picture)
-            binding?.uploadButtonImageView?.visibility = View.VISIBLE
+            binding?.extractTextButton?.visibility = View.VISIBLE
+            binding?.extractedTextCardView?.visibility = View.VISIBLE
         }
     }
 }
