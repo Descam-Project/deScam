1218b48957ba2dcf07d62ac006ee4cb98fad29cb
captvelsky <lesmananabil@gmail.com>
2022-06-06 06:26:54 +0700

    prepare retrofit

--- app/src/main/java/com/captvelsky/descam/di/AppModule.kt
+++ app/src/main/java/com/captvelsky/descam/di/AppModule.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
+import com.captvelsky.descam.data.remote.ApiConfig
+import com.captvelsky.descam.data.remote.ApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -19,4 +21,7 @@ object AppModule {
     @Provides
     fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> =
         context.dataStore
+
+    @Provides
+    fun provideApiService(): ApiService = ApiConfig.getApiService()
 }
